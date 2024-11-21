ea0b1285537a89e5db00843e13ecfdc5d93e09f6
Caleb Maddry <maddrycaleb@gmail.com>
2024-11-21 10:37:17 -0700

    Added edits and lazy newton

--- Final Project/Code/method_functions.py
+++ Final Project/Code/method_functions.py
@@ -43,49 +43,6 @@ def GradientDescent(x0, F, J, tol, Nmax):
     ier = 1
     return[xstar, ier, its, xStep]
 
-def GradientDescentnD(x0, F, J, tol, Nmax):
-
-    ''' Lazy Newton = use only the inverse of the Jacobian for initial guess'''
-    ''' inputs: x0 = initial guess, F = Function, J = Jacobian of F,tol = tolerance, Nmax = max its'''
-    ''' Outputs: xstar= approximate root, ier = error message, its = num its'''
-
-    # Checks to see if the function is already at the root
-    if norm(F(x0)) == 0: 
-        xstar = x0
-        ier = 0
-        its = 0
-        return
-
-
-    for its in range(Nmax):
-        # Evaluate J and compute its inverse     
-        Jeval = J(x0)
-        Jtranspose= np.transpose(Jeval)          
-    
-        # Evaluate F
-        Feval = F(x0)
-
-        # Find the step length
-        alpha = 0.8 # THIS IS SOMETHING WE WANT TO FIND AN IMPLEMENTATION TO
-
-        p0 = Jtranspose.dot(Feval)
-
-        # Calculate the step 
-        x1 = x0 - alpha * p0
-    
-        # If we found the root (to within the tolerance), 
-        # return it and 0 for the error message
-        if (norm(x1 - x0) < tol):
-           xstar = x1
-           ier = 0
-           return[xstar, ier, its]
-        
-        x0 = x1
-    
-    # If we didn't find the root, return the step and an error of 1
-    xstar = x1
-    ier = 1
-    return[xstar, ier, its]
 
 def NewtonDescent(x0, F, J, H, tol, Nmax):
 
@@ -135,6 +92,54 @@ def NewtonDescent(x0, F, J, H, tol, Nmax):
     ier = 1
     return[xstar, ier, its, xStep]
 
+def LazyNewtonDescent(x0, F, J, H, tol, Nmax):
+
+    ''' NewtonDescent: use only the inverse of the Jacobian for initial guess'''
+    ''' inputs: x0 = initial guess, F = Function, J = Jacobian of F, H = Hessian of F, tol = tolerance, Nmax = max its'''
+    ''' Outputs: xstar = approximate root, ier = error message, its = num its, xStep = xk for each step k'''
+
+    # Checks to see if the function is already at the root
+    if norm(F(x0)) == 0: 
+        xstar = x0
+        ier = 0
+        its = 0
+        return
+
+    xStep = [x0]
+
+    # Evaluates the Hessian and computes its inverse
+    Heval = H(x0)   
+    Hinv = inv(Heval)  
+
+    for its in range(Nmax):
+        # Evaluate J    
+        Jeval = J(x0)    
+
+        # Find the step length
+        p0 = Hinv.dot(Jeval)
+        dk = -p0
+
+        # alpha = backTrackingLineSearch(x0, F, Jeval, dk, p = 0.5, alpha=1, c=1e-4)
+        alpha = backTrackingLineSearch(x0, F, Jeval, dk)
+
+        # Calculate the step 
+        x1 = x0 - alpha*p0
+        xStep = xStep + [x1.tolist()]
+       
+        # If we found the root (to within the tolerance), 
+        # return it and 0 for the error message
+        if (norm(x1 - x0) < tol):
+           xstar = x1
+           ier = 0
+           return[xstar, ier, its, xStep]
+        
+        x0 = x1
+    
+    # If we didn't find the root, return the step and an error of 1
+    xstar = x1
+    ier = 1
+    return[xstar, ier, its, xStep]
+
 def backTrackingLineSearch(xk, F, grad_xk, dk, p = 0.5, c=1e-3):
 
     ''' backTrackingLineSearch: calculate optimal alpha using Armijo condition'''
@@ -145,7 +150,7 @@ def backTrackingLineSearch(xk, F, grad_xk, dk, p = 0.5, c=1e-3):
     alpha = 1.0
     
     # While the Amrijo condition is not satisfied, keep decreasing alpha
-    while F(xk + alpha * dk) < F(xk) + c * alpha * np.dot(grad_xk, dk): 
+    while F(xk + alpha * dk) > F(xk) + c * alpha * np.dot(grad_xk, dk): 
         
         # Update alpha
         alpha = p * alpha
--- Final Project/Code/plottingFunctions.py
+++ Final Project/Code/plottingFunctions.py
@@ -5,13 +5,18 @@ from functions import Himmelblau, HimmelblauJ, HimmelblauH
 
 
 def PlotHimmelblau(x0 = [-5, 20]):
+    # Initial conditions and other parameters
     tol = 1e-16; Nmax = 100000;
+
+    # Finds the roots with Gradient Descent and then returns and prints the output
     GDRoot, error, iterations, GDSteps = GradientDescent(x0, Himmelblau, HimmelblauJ, tol, Nmax)
     print("Root using Gradient Descent:", GDRoot, "\n\t Number of iterations:", iterations)
 
+    # Finds the roots with Newton Descent and then returns and prints the output
     NDRoot, error, iterations, NDSteps = NewtonDescent(x0, Himmelblau, HimmelblauJ, HimmelblauH, tol, Nmax)
     print("Root using Newton Descent:", NDRoot, "\n\t Number of iterations:", iterations)
 
+    # Converts the lists into numpy arrays for plotting
     GDSteps = np.array(GDSteps)
     NDSteps = np.array(NDSteps)
 
